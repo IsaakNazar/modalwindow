75604ea58b705e3eaf692eecdfb8bff162a2eed4
nisakov <nazarbek.isakov@zensoft.io>
2019-06-04 19:00:34 +0600

    forth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BodyOneComponent } from './body-one/body-one.component';
 import { BodyTwoComponent } from './body-two/body-two.component';
 import { ModalComponent } from './modal/modal.component';
 import {ModalService} from './modal.service';
+import {BodyThreeComponent} from './body-three/body-three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BodyOneComponent,
     BodyTwoComponent,
+    BodyThreeComponent,
     ModalComponent
   ],
   imports: [
---
+++ src/app/body-three/body-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-two',
+  templateUrl: './body-three.component.html',
+  styleUrls: ['./body-three.component.scss']
+})
+export class BodyThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
